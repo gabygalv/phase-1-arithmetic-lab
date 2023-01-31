9befc94db87d89365d1e04241824279541f73fe4
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-01-30 20:16:23 -0700

    completed arithmatic lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+{
+    let num1 = 2;
+    let num2 = 31;
+    let multiply = num1 * num2;
+}
+
+{
+    let random = Math.floor(Math.random() * 100);
+}
+
+{
+    let num3 = 18;
+    let num4 = 7;
+    let mod = num3 % num4;
+}
+
+{
+    let max = Math.max(3, 7, 14, 0, 20);
+}
